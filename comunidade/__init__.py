debc882e5b198c22c8dda82f7521e006a9480d84
Thiago Regueira <thiago.regueira@yahoo.com.br>
2024-01-23 14:57:49 -0300

    Add database creation logic to __init__.py

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 
 app = Flask(__name__)
@@ -19,5 +20,18 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "alert-info"
 
+
+# criar banco de dados postgresql no primeiro uso
+from comunidade import models  # noqa: E402, F401
+
+engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Banco de dados criado com sucesso!")
+else:
+    print("Banco de dados ja existe!")
 
 from comunidade import routes  # noqa: E402, F401
